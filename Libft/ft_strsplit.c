2aa9f77edfcc1f78d7a0c6368328e6cd7237387f
Artiom Gesp <artiomgesp@MacBook-Pro-de-Artiom.local>
2018-11-11 18:19:15 +0100

    split repare

--- Libft/ft_strsplit.c
+++ Libft/ft_strsplit.c
@@ -12,53 +12,7 @@
 
 #include "libft.h"
 
-static int	readstart(char const *s, char c, int startorend)
-{
-	int		i;
-
-	if (startorend == 1)
-	{
-		i = 0;
-		while (s[i] == c)
-			i++;
-		return (i);
-	}
-	else
-	{
-		i = ft_strlen(s) - 1;
-		while (s[i] == c)
-			i--;
-		return (i);
-	}
-}
-
-static char	*ft_strtrimop(char const *s, char c, int start, int end)
-{
-	int i;
-	char *save;
-
-	i = 0;
-	if (readstart(s, c, 1) == readstart(s, c, 2) && s[0] != '\0')
-	{
-		save = malloc(sizeof(char) * 2);
-		save[0] = s[readstart(s, c, 1)];
-		save[1] = '\0';
-		return (save);
-	}
-	if (start >= end)
-	{
-		save = ft_memalloc(1);
-		return (save);
-	}
-	if (!(save = (char*)malloc(sizeof(char) * (end - start + 2))))
-		return (NULL);
-	while (start <= end)
-		save[i++] = s[start++];
-	save[i] = '\0';
-	return (save);
-}
-
-static int		countword(char *s, char c)
+static int		countword(const char *s, char c)
 {
 	int i;
 	int nbmots;
@@ -82,7 +36,7 @@ static int		countword(char *s, char c)
 	return (0);
 }
 
-char	**do_split(char const *s, char c, char **myreturn, int countmots)
+static char	**do_split(char const *s, char c, char **myreturn, int countmots)
 {
 	int		i;
 	int		j;
@@ -118,9 +72,8 @@ char		**ft_strsplit(char const *s, char c)
 	if (s)
 	{
 		i = -1;
-		countmots = countword(ft_strtrimop(s, c, readstart(s, c, 1), 
-					readstart(s, c, 2)), c);
-		if (!(myreturn = (char**)malloc(sizeof(char*) * (countmots + 1))))
+		countmots = countword((const char *)s, c);
+		if (!(myreturn = (char**)malloc(sizeof(*myreturn) * (countmots + 1))))
 			return (NULL);
 		while (++i < countmots)
 			if (!(myreturn[i] = (char*)malloc(sizeof(char) * (ft_strlen(s) + 1))))
