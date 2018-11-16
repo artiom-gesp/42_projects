9d99a640151218cd4e673e9e6a72376f95cd0bf9
Artiom GESP <agesp@e3r6p6.42.fr>
2018-11-16 17:22:46 +0100

    debug get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,118 +1,161 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   new_get_next_line.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agesp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/13 11:46:03 by agesp             #+#    #+#             */
-/*   Updated: 2018/11/15 13:03:55 by agesp            ###   ########.fr       */
+/*   Created: 2018/11/15 14:11:04 by agesp             #+#    #+#             */
+/*   Updated: 2018/11/16 17:21:22 by agesp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "stdio.h"
+#include <stdio.h>
 
-char	*check_for_endline(char *buff, char **line, int *j, int *k)
+int ft_strccat(char *dst, const char* src, int c)
 {
-	size_t i = 0;
-	//char *save;
-	char	*my_return;
-	int flag = 0;
-	if (!(my_return = ft_strdup(buff)))
-		return (NULL);
-	while (buff[i] && !flag)
-	{
-		if (buff[i] == '\n')
-		{
-			my_return = ft_strncpy(my_return, buff, i + 1);
-			my_return[i] = '\0';
-			flag = 1;
-			*j = 1;
-		}
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (dst[i] != '\0')
 		i++;
-	}
-	*k = i;
-	if (flag == 0)
+	while (src[j] != '\0')
 	{
-		my_return = ft_strcpy(my_return, buff);
-		ft_strcat(*line, my_return);
+		dst[i + j] = src[j];
+		if (src[j] == c)
+		{
+			dst[i + j] = '\0';
+			return (1);
+		}
+		j++;
 	}
-	return (&my_return[i]);
+	dst[i + j] = '\0';
+	return (0);
 }
 
-char	*read_once(const int fd)
-{
-	int ret;
-	char *buff;
-
-	if (!(buff = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
-		return (NULL);
-	ret = 0;
-
-	ret = read(fd, buff, BUFF_SIZE);
-	if (ret == -1)
-		return (NULL);
-	if (ret == 0)
-		return ("-1");
-	buff[ret] = '\0';
-	return (buff);
-}
-
-char	*extract_line(char *save, char *save_rest)
+char	*ft_strccpy(char *dst, const char *src, int c)
 {
 	int i;
 
 	i = 0;
-	if (save_rest)
+	while (src[i])
 	{
-		save = ft_memccpy(save, save_rest, '\n', BUFF_SIZE);
+		if (src[i] == c)
+		{
+			dst[i] = '\0';
+			return (dst);
+		}
+		dst[i] = src[i];
+		i++;
 	}
-	return (save);
+	dst[i] = '\0';
+	return (dst);
 }
 
-int	get_next_line(const int fd, char **line)
+char *remove_start(char *save_my_buff)
 {
-	int j;
-	static int k = 0;
-	static char *save;
-	char *save_rest;
-	char fake_buff[1];
+	int i;
 
-	j = 0;
-	ft_memset(*line, '\0', sizeof(*line));
-	printf("%s\n", *line);
-	while (j == 0)
-	{
-		if (!(save_rest = check_for_endline(read_once(fd), line, &j, &k)))
-			return (0);
-		if (save)
-			ft_strcat(save, save_rest);
-		else
-			save = ft_strdup(*line);
-//		save = extract_line(save, save_rest);
-	}
-	printf("%s\n", *line);
-	if (!(read(fd, fake_buff, 0)))
+	i = 0;
+	while (save_my_buff[i] && save_my_buff[i] != '\n')
+		i++;
+		return (char*)&save_my_buff[i + 1];
+}
+
+char	*ft_strcdup(char const *s, int c)
+{
+	char	*dst;
+	int		i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	if (!(dst = malloc(sizeof(char) * (i + 1))))
 		return (0);
+	i = 0;
+	while (s[i] != c)
+	{
+		dst[i] = s[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
 
-	return (1);
+int		get_next_line(int const fd, char **line)
+{
+	int ret;
+	char buff[BUFF_SIZE + 1];
+	static char *save_my_buff;
+	ret = 1;
+	while (ret > 0)
+	{
+			if (save_my_buff)
+			{
+				if (ft_strchr(save_my_buff, '\n'))
+				{
+					*line = ft_strccpy(save_my_buff, save_my_buff, '\n');
+					save_my_buff = remove_start(save_my_buff);
+					return (1);
+				}
+				else
+				{
+					ret = read(fd, buff, BUFF_SIZE);
+					buff[ret] = '\0';
+					ft_strccat(save_my_buff, buff, '\0');
+				}
+			}
+			else
+			{
+				save_my_buff = ft_memalloc(sizeof(buff));
+			}
+	}
+	return (0);
 }
 
 int	main(int ac, char **av)
-{
-	int fd;
-	char *baobab;
+{/*
+	char *str;
 
-	baobab = ft_memalloc(1000000);
-	if (ac > 1)
+//	str = ft_memalloc(1111111);
+	if (ac > 2)
 	{
-		int i= -1;
-		fd = open(av[1], O_RDONLY);
+		int i = -1;
+		int fd = open(av[1], O_RDONLY);
 		while (++i < ft_atoi(av[2]))
 		{
-			get_next_line(fd, &baobab);
+			get_next_line(fd, &str);
+			printf("%s\n", str);
 		}
 	}
-	free(baobab);
+*/
+	char		*line;
+	int			fd;
+	int			ret;
+	int			count_lines;
+	int			errors;
+
+	fd = open("test", O_RDONLY);
+	count_lines = 0;
+	errors = 0;
+	line = NULL;
+	while ((ret = get_next_line(fd, &line)) > 0)
+	{
+		if (count_lines == 0 && strcmp(line, "1234567") != 0)
+			errors++;
+		if (count_lines == 1 && strcmp(line, "abcdefg") != 0)
+			errors++;
+		if (count_lines == 2 && strcmp(line, "4567890") != 0)
+			errors++;
+		if (count_lines == 3 && strcmp(line, "defghij") != 0)
+			errors++;
+		count_lines++;
+		if (count_lines > 50)
+			break ;
+//		printf("%d\n", errors);
+	}
+	printf("%d\n", count_lines);
 }
