025040f9df3f8e78764c30c07301c2614381a216
Artiom Gesp <artiomgsp@gmail.com>
2019-07-05 15:28:57 +0200

    added comments to onevsall.py

--- Python_Projects/machine_learning/OneVsAll.py
+++ Python_Projects/machine_learning/OneVsAll.py
@@ -18,6 +18,7 @@ m, n = X.shape
 
 
 def get_rand_image(dataset):
+    """print a single example from the dataset"""
     img_pos = np.random.randint(low=5000, size=1)
     img = np.reshape(dataset[img_pos], (20, 20))
     plt.imshow(np.transpose(img))
@@ -30,6 +31,7 @@ def sigmoid(var):
 
 
 def one_vs_all(X, y, lmbda, num_class):
+    """train params theta for each class and store them in an array of size nb_of_class x pixel_number per image"""
     theta = np.zeros((k, n))
     for i in range(num_class):
         digit_class = i
@@ -40,37 +42,51 @@ def one_vs_all(X, y, lmbda, num_class):
 
 
 def predict(img, theta):
+    """compute the likelihood for the image to belong to each class (0-9) than returns the higher percentage"""
     num_class = [None] * theta.shape[0]
     for i in range(0, 10):
         cur_theta = np.reshape(theta[i, :], (theta.shape[1], 1))
         num_class[i] = sigmoid(np.dot(img, cur_theta))[0][0]
-    print(np.argmax(np.array(num_class)))
+    return np.argmax(np.array(num_class))
 
 
 def get_gradient(theta, x_val, y_val, lmda):
     # m = len(y)
     temp = sigmoid(np.dot(x_val, theta)) - y_val
     # temp = np.dot(temp.T, X).T
-    temp = np.dot(X.T, temp)
+    temp = np.dot(X.T, temp) + (lmbda / m) * theta
     return temp
 
 
 def get_cost(theta, x_val, y, lmda):
+    """cost function for logistic regression"""
     m = len(y)
     temp1 = np.multiply(y, np.log(sigmoid(np.dot(x_val, theta))))
     temp2 = np.multiply(1 - y, np.log(1 - sigmoid(np.dot(x_val, theta))))
-    return np.sum(temp1 + temp2) / -m
+    return np.sum(temp1 + temp2) / -m + lmbda / (2 * m) * np.dot(np.transpose(theta), theta)
 
 
+def get_accuracy(theta, x_val, y):
+    accuracy = 0
+    for i in range(x_val.shape[0]):
+        img = np.transpose(np.reshape(x_val[i, :], (x_val[i, :].shape[0], 1)))
+        accuracy += 1 if predict(img, theta) == y[i] else 0
+    print('accuracy :', accuracy / x_val.shape[0], '%')
+
+
+"""lmbda for regularization and k the number of class of digits (0-9)"""
 lmbda = 0.1
 k = 10
 
 y = np.reshape(y, (5000, 1))
 
 theta = one_vs_all(X, y, lmbda, k)
+get_accuracy(theta=theta, x_val=X, y=y)
 
-for i in range(100):
-    img = get_rand_image(X[:, 1:])
-    img = np.insert(img, 0, 1)
-    predict(np.transpose(np.reshape(img, (img.shape[0], 1))), theta)
-time.sleep(0.5)
+
+"""print images of digits with corresponding prediction"""
+# for i in range(100):
+#     img = get_rand_image(X[:, 1:])
+#     img = np.insert(img, 0, 1)
+#     predict(np.transpose(np.reshape(img, (img.shape[0], 1))), theta)
+# time.sleep(0.5)
