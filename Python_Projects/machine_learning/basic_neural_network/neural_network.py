5dbf48b40e91a7151a71504de2c560b17311f7c1
Artiom Gesp <artiomgsp@gmail.com>
2019-07-10 16:43:00 +0200

    implementing back propagation algorithme

--- Python_Projects/machine_learning/basic_neural_network/neural_network.py
+++ Python_Projects/machine_learning/basic_neural_network/neural_network.py
@@ -2,75 +2,139 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as sp
 import pandas as pd
+import scipy.optimize as opt
 
 
 class NeuralNetwork:
 
-    def __init__(self, train_ex, labels, reg, theta: list = None):
+    def __init__(self, train_ex, labels, reg, weights: list = None, num_iters = 1500):
         self.input_layer_size = 400
         self.hidden_layer_size = 25
-        self.num_labels = 10
-        self.theta = self.init_theta(theta)  # theta = [np.array(), np.array()]
-        try:
-            """add bias column to training examples"""
-            self.train_ex = np.column_stack((np.ones(train_ex.shape[0]), train_ex))
-        except Exception:
-            print("Error adding bias column to training examples, please verify your input")
-            exit(-1)
-        self.labels = labels
-        self.reg = reg  # regularization coefficient for cost and gradient
+        self.num_labels = 10  # output layer size
+        self.weights_bias = self.init_weights_biases(weights)  # weights = [np.array(),... ,np.array()]
 
-    def init_theta(self, theta: list = None) -> list:
-        """randomly initialize theta if no (correct) parameters were provided"""
-        if not theta:
-            init_1 = np.sqrt(6) / np.sqrt(self.input_layer_size + self.hidden_layer_size)
-            init_2 = np.sqrt(6) / np.sqrt(self.hidden_layer_size + self.num_labels)
-            theta_1 = np.random.uniform(-init_1, init_1, (self.hidden_layer_size, self.input_layer_size + 1))
-            theta_2 = np.random.uniform(-init_2, init_2, (self.num_labels, self.hidden_layer_size + 1))
-            print(init_1, init_2)
-            return [theta_1, theta_2]
-        try:
-            if (theta[0].shape != (self.hidden_layer_size, self.input_layer_size + 1) \
-                    or theta[1].shape != (self.num_labels, self.hidden_layer_size + 1)):
+        self.train_ex = train_ex
+        self.labels = labels
+
+        self.reg = reg  # regularization coefficient for cost and gradient
+        self.num_iters = 50
+
+    def is_input_valid(self, weights):
+        try:  # check user provided weight_bias list
+            error_flag = False
+            if weights[0].shape != (self.hidden_layer_size, self.input_layer_size):
+                error_flag = True
+            if weights[1].shape != (self.num_labels, self.hidden_layer_size):
+                error_flag = True
+            if weights[2].shape != (1, self.hidden_layer_size):
+                error_flag = True
+            if weights[3].shape != (1, self.num_labels):
+                error_flag = True
+
+            if error_flag:
                 print("Incorrect params shape")
-                exit(-1)
+                return False
         except Exception:
             print('Incorrect parameters')
-            exit(-1)
-        return theta
+            return False
+        return True
+
+    def init_weights_biases(self, weights: list = None) -> list:
+        """randomly initialize weights if no (correct) parameters were provided"""
+        if not weights:  # if no weights_bias list were provided, initialize it randomly
+            init_1 = np.sqrt(6) / np.sqrt(self.input_layer_size + self.hidden_layer_size)  # defines min/max
+            init_2 = np.sqrt(6) / np.sqrt(self.hidden_layer_size + self.num_labels)  # for each parameter's values
+
+            weights_1 = np.random.uniform(-init_1, init_1, (self.hidden_layer_size, self.input_layer_size))
+            weights_2 = np.random.uniform(-init_2, init_2, (self.num_labels, self.hidden_layer_size))
+
+            bias_1 = np.random.uniform(-init_2, init_2, (1, self.hidden_layer_size))
+            bias_2 = np.random.uniform(-init_2, init_2, (1, self.num_labels))
+
+            return [weights_1, weights_2, bias_1, bias_2]
+
+        if not self.is_input_valid(weights):
+            print('Initializing weights randomly..')
+            return self.init_weights_biases()
+
+        return weights  # if no errors were raised, returns the same list
+
+
+    @staticmethod
+    def forward_prop(weights, train_ex):
+        weights_1 = weights[0]  # 25 * 400
+        weights_2 = weights[1]  # 10 * 25
+        bias_1 = weights[2]  # 1 * 25
+        bias_2 = weights[3]  # 1 * 10
+
+        #   bias_1 is a row vector, added to every row of a_1
+        z_1 = np.dot(train_ex, weights_1.T) + bias_1
+        a_1 = NeuralNetwork.sigmoid(z_1)  # 5000 * 25
+
+        z_2 = np.dot(a_1, weights_2.T) + bias_2
+        return NeuralNetwork.sigmoid(z_2), a_1, z_2, z_1  # 5000 * 10
 
     @staticmethod
     def sigmoid(x, derivative=False):
         if derivative:
-            return np.exp(-x) / (1 + np.exp(-x))**2
+            return np.exp(-x) / ((1 + np.exp(-x)) ** 2)
         return 1 / (1 + np.exp(-x))
 
     @staticmethod
-    def forward_prop(theta, train_ex):
+    def compute_cost(weights, train_ex, labels, reg):  # cost function of neural network
+
         num_train_ex = train_ex.shape[0]
-        theta_1 = theta[0]  # 25 * 401
-        theta_2 = theta[1]  # 10 * 26
+        weights_1 = weights[0]
+        weights_2 = weights[1]
 
-        a_1 = NeuralNetwork.sigmoid(np.dot(train_ex, theta_1.T))    # 5000 * 25
-        a_1 = np.hstack((np.ones((num_train_ex, 1)), a_1))  # 5000 * 26
+        y = pd.get_dummies(labels.flatten())  # 5000 * 1
+        h, a_1, z_2, z_1 = NeuralNetwork.forward_prop(weights, train_ex)  # 5000 * 10
 
-        return NeuralNetwork.sigmoid(np.dot(a_1, theta_2.T))    # 5000 * 10
+        temp1 = np.multiply(y, np.log(h))  # 5000 * 10
+        temp2 = np.multiply(1 - y, np.log(1 - h))  # 5000 * 10
+        cost = np.sum(temp1 + temp2)  # 1 * 1
+
+        reg_cost_1 = np.sum(weights_1 ** 2)
+        reg_cost_2 = np.sum(weights_2 ** 2)
+
+        return np.sum(cost / (-num_train_ex)) + reg / (2.0 * num_train_ex) * (reg_cost_1 + reg_cost_2)
 
     @staticmethod
-    def compute_cost(theta, train_ex, labels, reg):
-        """cost function of neural network"""
+    def compute_gradient(weights_bias, train_ex, labels, reg):
         num_train_ex = train_ex.shape[0]
-        theta_1 = theta[0]
-        theta_2 = theta[1]
 
-        y = pd.get_dummies(labels.flatten())    # 5000 * 1
-        h = NeuralNetwork.forward_prop(theta, train_ex)     # 5000 * 10
+        weights_1 = weights_bias[0]  # 25 * 400
+        weights_2 = weights_bias[1]  # 10 * 25
+        bias_1 = weights_bias[2]  # 1 * 25
+        bias_2 = weights_bias[3]  # 1 * 10
 
-        temp1 = np.multiply(y, np.log(h))     # 5000 * 10
-        temp2 = np.multiply(1 - y, np.log(1 - h))     # 5000 * 10
-        cost = np.sum(temp1 + temp2)    # 1 * 1
+        delta_1 = np.zeros(weights_1.shape)
+        delta_2 = np.zeros(weights_2.shape)
+        update_b1 = np.zeros(bias_1.shape)
+        update_b2 = np.zeros(bias_2.shape)
 
-        reg_thet_1 = np.sum(theta_1[:, 1:]**2)
-        reg_thet_2 = np.sum(theta_2[:, 1:]**2)
+        labels = pd.get_dummies(labels.flatten())
+        a_2, a_1, z_2, z_1 = NeuralNetwork.forward_prop(weights_bias, train_ex)
 
-        return np.sum(cost / (-num_train_ex)) + reg / (2.0 * num_train_ex) * (reg_thet_1 + reg_thet_2)
+        d_2 = a_2 - np.array(labels)  # 5000 * 10
+        d_1 = np.dot(d_2, weights_2)    # 5000 * 25
+        d_1 = np.multiply(d_1, NeuralNetwork.sigmoid(z_1, derivative=True))
+
+        delta_1 += d_1.T @ train_ex
+        delta_2 += d_2.T @ a_1
+
+        delta_1 /= num_train_ex
+        delta_2 /= num_train_ex
+
+        delta_1 += weights_1 * reg / num_train_ex
+        delta_2 += weights_2 * reg / num_train_ex
+
+        update_b2 = np.sum(d_2, axis=0)
+        update_b1 = np.sum(d_1, axis=0)
+
+        return [delta_1, delta_2, update_b1, update_b2]
+
+    def learn_param(self):
+        self.weights_bias = opt.fmin_cg(f=NeuralNetwork.compute_cost, x0=self.weights_bias,
+                                        args=(self.train_ex, self.labels, self.reg), maxiter=200)
+        return self.weights_bias
--- Python_Projects/machine_learning/basic_neural_network/tests/nn_cost_unit_tests.py
+++ Python_Projects/machine_learning/basic_neural_network/tests/nn_cost_unit_tests.py
@@ -4,24 +4,37 @@ sys.path.append("..")
 import basic_neural_network.neural_network as nn
 import numpy as np
 import scipy.io as sp
+import pandas as pd
 
 mat = sp.loadmat(os.getcwd() + '/ressources/ex4data1.mat')
 data = np.hstack((mat['X'], mat['y']))
 
 X = np.array(mat['X'])  # training examples : 20px x 20px images mapped as rows of 400 elements
 y = np.array(mat['y'])  # corresponding label : (1 to 10, 10 being the label for 0 digits)
-# y[y == 10] = 0  # original dataset has label 10 for 0 digits
 
-X = np.column_stack((np.ones(X.shape[0]), X))   # add bias column to training examples
+# y[y == 10] = 0
 
 test_theta = sp.loadmat(os.getcwd() + '/ressources/ex4weights.mat')
 
 theta_one = np.array(test_theta['Theta1'])
 theta_two = np.array(test_theta['Theta2'])
 
-global_theta = [theta_one, theta_two]
-print(nn.NeuralNetwork.compute_cost(global_theta, X, y, 1))
-# print(nn.NeuralNetwork.sigmoid(0))
-print(nn.NeuralNetwork.sigmoid(0, derivative=True))
+weights_one = theta_one[:, 1:]
+weights_two = theta_two[:, 1:]
 
-nn.NeuralNetwork(X, y, 1)
+bias_one = np.reshape(theta_one[:, 0], (1, weights_one.shape[0]))
+bias_two = np.reshape(theta_two[:, 0], (1, weights_two.shape[0]))
+
+weights_bias = [weights_one, weights_two, bias_one, bias_two]
+
+# nnn = nn.NeuralNetwork(X, y, 1, weights_bias)
+nnn = nn.NeuralNetwork(X, y, 1)
+
+# print(nn.NeuralNetwork.compute_cost(nnn.weights_bias, nnn.train_ex, y, 1))  # expected 0.3837
+
+# res = nnn.compute_gradient()
+print(nnn.learn_param())
+
+# test = pd.get_dummies(y.flatten())
+# test = np.array(test)
+# print(test[0])
